6876391f1f384509dbf9dfe3541354c8f8f5251b
Pabl0PR <pablo.rego104@gmail.com>
2025-05-26 19:10:02 +0200

    Final touch

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, count_character, dic_to_sorted_list
 
 def get_book_text(filePath):
@@ -8,7 +9,7 @@ def get_book_text(filePath):
 
 def printReport(bookPath, word_count):
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {bookPath}.txt...")
+    print(f"Analyzing book found at {bookPath}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
@@ -16,13 +17,19 @@ def printReport(bookPath, word_count):
     
 def main():
 
-    book_text = get_book_text('books/frankenstein.txt')
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
+
+    book_text = get_book_text(book_path)
     book_word_count = count_words(book_text)
 
     book_character_count = count_character(book_text)
     #print(book_character_count)
 
-    printReport("books/frankenstein", book_word_count)
+    printReport(book_path, book_word_count)
     result = dic_to_sorted_list(book_character_count)
     for element in result:
         if element["char"].isalpha():
